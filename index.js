0934ea79e8bde701013640f4530a84b09518059b
LoopFruits <Kensworldc@gmail.com>
2022-08-07 10:12:56 -0400

    commit tree 900c7febc55a5141cf8f83020da27b6901a99386

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-function scuberGreetingForFeet(feet) {
-  if (feet <= 400) {
-    return "This one is on me!";
+function scuberGreetingForFeet(feet){
+  if (199 >= feet){
+    return 'This one is on me!';
+  } 
+    else if (feet >= 2000 && feet < 2500) {
+      return 'I will gladly take your thirty bucks.';
+    
   }
-    else if ( feet >= 2000 && feet < 2500) {
-      return "I will gladly take your thirty bucks.";
-    }
-    else if ( feet >= 2500) {
-      return 'No can do.';
-    }
+    else 
+    return 'No can do.'
+  
 }
 
 function ternaryCheckCity(city){
-  return (city == "NYC" ? "Ok, sounds good." : "No go.");
-}
-
-function switchOnCharmFromTip(tip){
-  switch(tip) {
-    case "generous" :
-    return "Thank you so much." ;
-    case "not as generous" :
-    return "Thank you." ;
-    default: return "Bye.";
-  }
+  return (city === 'NYC' ? 'Ok, sounds good' : 'No go.')
 }
